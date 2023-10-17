d06d7e13c2703531eb8ac4bed5ed80fe984ce045
angel.z <borsi678@ciencias.unam.mx>
2023-10-16 19:55:25 -0600

    feature crud category

--- src/app/modules/product/components/category/category.component.ts
+++ src/app/modules/product/components/category/category.component.ts
@@ -10,6 +10,7 @@ declare var $: any;
 })
 export class CategoryComponent {
   categories: Category[] = [];
+  categoryUpdated: number = 0;
 
   ngOnInit(){
     this.getCategories();
@@ -37,11 +38,20 @@ export class CategoryComponent {
 
   showModalForm(){
     this.form.reset();
+    this.categoryUpdated = 0;
     this.submitted = false;
     $("#modalForm").modal("show");
   }
 
   onSubmit(){
+    if(this.categoryUpdated == 0){
+      this.onSumbitCreate();
+      return;
+    }
+    this.onSubmitUpdate();
+  }
+
+  onSumbitCreate(){
     this.submitted = true;
 
     if(this.form.invalid) return;
@@ -54,7 +64,58 @@ export class CategoryComponent {
     $("#modalForm").modal("hide");
 
     alert("Categoria guardada exitosamente!");
+    this.categoryUpdated = 0;
+  }
 
+  onSubmitUpdate(){
+    this.submitted = true;
+
+    if(this.form.invalid) return;
+
+    this.submitted = false;
+
+    for(let category of this.categories){
+      if(category.category_id == this.categoryUpdated){
+        category.category = this.form.controls['category'].value!;
+        category.code = this.form.controls['code'].value!
+        break;
+      }
+    }
+    $("#modalForm").modal("hide");
+
+    alert("Categoria actualizada exitosamente!");
+
+    this.categoryUpdated = 0;
+  }
+
+  updateCategory(category : Category){
+    this.categoryUpdated = category.category_id;
+
+    this.form.reset();
+    this.form.controls['category'].setValue(category.category);
+    this.form.controls['code'].setValue(category.code);
+
+    this.submitted = false;
+    $("#modalForm").modal("show");
+  }
+  disableCategory(id: number){
+    for(let category of this.categories){
+      if(category.category_id == id){
+        category.status = 0;
+        alert("Región desactivada exitosamente!");
+        break;
+      }
+    }
+  }
+
+  enableCategory(id: number){
+    for(let category of this.categories){
+      if(category.category_id == id){
+        category.status = 1;
+        alert("Región activada exitosamente!");
+        break;
+      }
+    }
   }
 
 }
