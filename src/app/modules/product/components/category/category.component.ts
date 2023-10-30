22c601734503c620670763dbd099cd10a594646d
angel.z <borsi678@ciencias.unam.mx>
2023-10-29 23:43:10 -0600

    feature CRUD with API

--- src/app/modules/product/components/category/category.component.ts
+++ src/app/modules/product/components/category/category.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Category } from '../../_models/category';
 import { FormBuilder, Validators } from '@angular/forms';
+import {CategoryService} from '../../_services/category.service';
+import {resolve} from "@angular/compiler-cli";
 declare var $: any;
 
 @Component({
@@ -10,112 +12,117 @@ declare var $: any;
 })
 export class CategoryComponent {
   categories: Category[] = [];
-  categoryUpdated: number = 0;
+  categoryUpdated: boolean = false; // true si la categoria se actualiza, false si se es nueva
+  submitted : boolean= false;
 
   ngOnInit(){
     this.getCategories();
   }
 
   form = this.formBuilder.group({
+    id: [0, []],
     category: ["", [Validators.required]],
     code: ["", [Validators.required]],
   });
 
-  submitted = false;
 
-  constructor(private formBuilder: FormBuilder) {
-
-  }
+  constructor( private categoryService: CategoryService,
+               private formBuilder: FormBuilder) {}
   getCategories(){
-    let category1 = new Category(1, "ABCDE", "Uno", 1);
-    let category2 = new Category(2, "ABCDF", "Dos", 1);
-    let category3 = new Category(3, "ABCDG", "Tres", 0);
-
-    this.categories.push(category1);
-    this.categories.push(category2);
-    this.categories.push(category3);
+    this.categoryService.getCategories().subscribe(
+      res => {
+        this.categories=res;
+        console.log("categorias cargadas")
+      },
+      err => {
+        console.log("error al cargar categorias")
+      }
+    );
   }
 
   showModalForm(){
     this.form.reset();
-    this.categoryUpdated = 0;
-    this.submitted = false;
+    this.categoryUpdated=false;
+    this.submitted=false;
     $("#modalForm").modal("show");
   }
-
-  onSubmit(){
-    if(this.categoryUpdated == 0){
-      this.onSumbitCreate();
-      return;
-    }
-    this.onSubmitUpdate();
-  }
-
-  onSumbitCreate(){
-    this.submitted = true;
-
-    if(this.form.invalid) return;
-
-    this.submitted = false;
-
-    let category = new Category(0, this.form.controls['code'].value!, this.form.controls['category'].value!, 1);
-    this.categories.push(category);
-
-    $("#modalForm").modal("hide");
-
-    alert("Categoria guardada exitosamente!");
-    this.categoryUpdated = 0;
-  }
-
-  onSubmitUpdate(){
-    this.submitted = true;
-
-    if(this.form.invalid) return;
-
-    this.submitted = false;
-
-    for(let category of this.categories){
-      if(category.category_id == this.categoryUpdated){
-        category.category = this.form.controls['category'].value!;
-        category.code = this.form.controls['code'].value!
-        break;
-      }
-    }
-    $("#modalForm").modal("hide");
-
-    alert("Categoria actualizada exitosamente!");
-
-    this.categoryUpdated = 0;
-  }
-
-  updateCategory(category : Category){
-    this.categoryUpdated = category.category_id;
-
+  showModalFormUpdated (category : Category){
+    this.categoryUpdated=true;
+    this.submitted=false;
     this.form.reset();
+    this.form.controls['id'].setValue(category.category_id);
     this.form.controls['category'].setValue(category.category);
     this.form.controls['code'].setValue(category.code);
-
-    this.submitted = false;
     $("#modalForm").modal("show");
+
   }
-  disableCategory(id: number){
-    for(let category of this.categories){
-      if(category.category_id == id){
-        category.status = 0;
-        alert("Región desactivada exitosamente!");
-        break;
-      }
+  onSubmit(){
+    this.submitted=true;
+    if(!this.categoryUpdated){
+      this.createCategory();
+      return;
     }
+    this.updateCategory(this.form.value);
+  }
+
+  createCategory(){
+    if(this.form.invalid) return;
+
+    this.categoryService.createCategory(this.form.value).subscribe(
+      res => {
+        console.log("creado categoria")
+        this.getCategories(); //consulta las categorias actualizadas
+        $("#modalForm").modal("hide"); //oculta el modal
+      },
+      error => {
+        console.log("fallo agregar categoria")
+      }
+    )
+  }
+
+
+  updateCategory(category : any){
+    if(this.form.invalid) return;
+    console.log(category);
+    this.categoryService.updateCategory(category,category.id).subscribe(
+      res => {
+
+        this.getCategories();
+        console.log("categoria actualizada");
+        $("#modalForm").modal("hide");
+      },
+      error => {
+        console.log("error al actualziar categoria");
+      }
+    )
+
+  }
+
+
+  disableCategory(id: number){
+    this.categoryService.disableCategory(id).subscribe(
+      res => {
+        console.log("categoria deshabilitada");
+
+        this.getCategories();
+      },
+      error => {
+        console.log("error al deshabilitar categorias");
+      }
+    );
   }
 
   enableCategory(id: number){
-    for(let category of this.categories){
-      if(category.category_id == id){
-        category.status = 1;
-        alert("Región activada exitosamente!");
-        break;
+    this.categoryService.enableCategory(id).subscribe(
+      res => {
+        console.log("categoria habilitada");
+
+        this.getCategories();
+      },
+      error => {
+        console.log("error al habilitar categorias");
       }
-    }
+    );
   }
 
 }
