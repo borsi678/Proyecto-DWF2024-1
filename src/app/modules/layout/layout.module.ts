93844fe607a60e466ffdc522b97041148babfc21
angel.z <borsi678@ciencias.unam.mx>
2023-12-11 18:54:30 -0600

    feature footer view

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import {RouterModule} from "@angular/router";
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
-  exports: [
-    NavbarComponent
-  ],
+    exports: [
+        NavbarComponent,
+        FooterComponent
+    ],
   imports: [
     CommonModule,
     RouterModule
